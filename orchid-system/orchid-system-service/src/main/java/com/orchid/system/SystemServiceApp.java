ac3245371b281861d86870124d813d4936972ae1
biejunyang <biejunyang@genomics.cn>
2021-04-09 14:48:53 +0800

    axx

--- orchid-system/orchid-system-service/src/main/java/com/orchid/system/SystemServiceApp.java
+++ orchid-system/orchid-system-service/src/main/java/com/orchid/system/SystemServiceApp.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableResourceServer
 @SpringBootApplication
 @RestController
 public class SystemServiceApp {
--- orchid-system/orchid-system-service/src/main/java/com/orchid/system/config/ResourceServerConfig.java
+++ orchid-system/orchid-system-service/src/main/java/com/orchid/system/config/ResourceServerConfig.java
@@ -1,12 +1,16 @@
 package com.orchid.system.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
+@EnableResourceServer
 @Configuration
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
@@ -15,7 +19,15 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        super.configure(resources);
-        resources.tokenStore(new RedisTokenStore(redisConnectionFactory));
+//        super.configure(resources);
+        resources.tokenStore(tokenStore());
+    }
+
+
+    @Bean
+    public TokenStore tokenStore(){
+        RedisTokenStore tokenStore=new RedisTokenStore(redisConnectionFactory);
+        tokenStore.setPrefix("orchid:auth-server");
+        return tokenStore;
     }
 }
